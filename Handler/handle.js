9e7341cf7f7add90e70277c6ee2056338470cd91
GauravMendix0 <gaurav.gokhale@mendix.com>
2025-06-16 13:31:10 +0530

    fix(handler): update handler function as per api guidelines

--- Handler/handle.js
+++ Handler/handle.js
@@ -2,17 +2,20 @@ const {StudentDB}=require("../model/student");
 
 async function handleGet(req,res)
 {
-
+    const id=req.params.id;
+    console.log(id);
     const stud=await StudentDB.findOne({
-        studentRoll:1
+        studentRoll:id
     });
     if(stud!=null)
     {
-        res.json(stud);
+        res.status(200).json(stud);
     }
     else
     {
-    res.send("No entry in DB");
+    res.status(404).json({
+        "message":"404 Not found"
+    });
     }
 
 }
@@ -22,8 +25,21 @@ async function handlegetall(req,res)
     await StudentDB.find().then((stud=>{
             res.status(200).json(stud);
         })).catch(()=>{
-            res.status(500).send("No entry in DB");
+            res.status(400).send("No entry in DB");
+        });
+
+    const stud=await StudentDB.find();
+
+    if(stud !=null)
+    {
+        res.status(200).json(stud)
+    }
+    else
+    {
+        res.status(404).json({
+            "message":"No entry in DB"
         })
+    }
 }
 
 async function handleCreate(req,res)
@@ -36,49 +52,113 @@ async function handleCreate(req,res)
         }
     ).then((stud)=>{
             console.log("New entry added in DB"+ stud);
-            res.status(200).send("new entry added in DB");
-    }).catch(()=>{
-        console.log("Error in adding entry in DB");
-        res.status(400).send("Error");
+            res.status(201).json({
+                "message":"New entry added in DB"
+            });
+    }).catch(async ()=>{
+
+        const st= await StudentDB.findOne({
+            studentRoll:req.body.studentRoll
+        });
+
+        if(st!=null)
+        {
+            res.status(409).json({
+                "message":"Roll number already exists"
+            })
+        }
+        else{
+            res.status(400).json({
+                "message":"Failed to add entry in DB"
+            })
+        }
     });
 }
 
 async function handleUpdate(req,res)
 {
-    await StudentDB.findOneAndUpdate({studentRoll:req.body.studentRoll},{studentName:req.body.studentName} 
-    ).then(()=>{
-        console.log("Entry updated in DB");
-        res.status(200).send("Entry updated in DB");
+    const id =req.params.id;
+    await StudentDB.findOneAndUpdate({studentRoll:id},{studentName:req.body.studentName} 
+    ).then((stud)=>{
+        
+        if(stud!=null)
+        {
+            console.log("Entry updated in DB");
+            res.status(200).json({
+            "message":"Entry updated in DB"
+            })  
+        }
+        else{
+            res.status(204).json({
+                "message":"No entry found in DB"
+            })
+        }
+
+        
     }).catch(()=>{
         console.log("Error in updating entry in DB");
-        res.status(400).send("Error");
+        res.status(400).json(
+            {
+                "message":"Error in updating entry in DB"
+            }
+        );
     });
 }
 
 async function handleUpdateAll(req,res)
 {
-    await StudentDB.findOneAndUpdate({studentRoll:req.body.studentRoll},
+    const id =req.params.id;
+    await StudentDB.findOneAndUpdate({studentRoll:id},
         {studentName:req.body.studentName, studentAge:req.body.studentAge, 
             studentBranch:req.body.studentBranch
         } 
-    ).then(()=>{
-        console.log("Entry updated in DB");
-        res.status(200).send("Entry updated in DB");
+    ).then((stud)=>{
+        if(stud!=null)
+        {
+            console.log("Entry updated in DB");
+            res.status(200).json({
+                "message":"Entry updated in DB"
+            })
+        }
+        else
+        {
+            res.status(204).json({
+                "message":"No entry found in DB"
+            })
+        }
+        
     }).catch(()=>{
         console.log("Error in updating entry in DB");
-        res.status(400).send("Error");
+        res.status(400).json({
+            "message":"Error in updating entry in DB"
+        });
     });
 }
 
 async function handleDelete(req,res)
 {
-    await StudentDB.findOneAndDelete({studentRoll:req.body.studentRoll}).
-        then(()=>{
-            console.log("Entry deleted in DB");
-            res.send("Entry deleted in DB");
+    const id =req.params.id;
+    await StudentDB.findOneAndDelete({studentRoll:id}).
+        then((stud)=>{
+            if(stud!=null)
+            {
+                console.log("Entry deleted in DB");
+                res.status(200).json({
+                    "message":"Entry deleted in DB"
+                })
+            }
+            else
+            {
+                res.status(202).json({
+                    "message":"No entry found in DB"
+                })
+            }
+            
         }).catch(()=>{
             console.log("Error in deleting entry in DB");
-            res.send("Error occured");
+            res.status(400).json({
+                "message":"Error in deleting entry in DB"
+            });
         });
 }
 
